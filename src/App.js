26061a3d793aab73fa41ca9ab4c2a72251511a7c
samirlamsal5 <samirlamsal5@gmail.com>
2024-05-11 21:16:48 +0545

    magintop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './components/landing';
+
+function App() {
+  return (
+    <div className=''>
+      <Landing/>
+    </div>
+  
+  );
+}
+
+export default App;

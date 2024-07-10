5034354a39a1797e635b624fadf68badce3a4a27
samirlamsal5 <samirlamsal5@gmail.com>
2024-07-10 13:37:51 +0545

    design changes

--- src/components/cursorfollower.jsx
+++ src/components/cursorfollower.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
 
+const CursorFollower = ({ children }) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [mouseConnected, setMouseConnected] = useState(true);
 
-const CursorFollower = ({children}) =>{
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-
-    useEffect(()=>{
-
+  useEffect(() => {
     const handleMouseMove = (event) => {
-        setPosition({ x: event.clientX, y: event.clientY });
-      };
-  
-      window.addEventListener('mousemove', handleMouseMove);
-  
-      return () => {
-        window.removeEventListener('mousemove', handleMouseMove);
-      };
-    },[])
-    return(
-        <div  className='cursor-follower' style={{ left: `${position.x}px`, top: `${position.y}px` }}>
-        </div>
-    )
-}
+      setPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    const detectMouse = () => {
+      if (navigator.maxTouchPoints === 0) {
+        // No touch points means a mouse is likely present
+        setMouseConnected(true);
+      } else {
+        setMouseConnected(false);
+      }
+    };
+
+    detectMouse();
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  if (!mouseConnected) {
+    return null;
+  }
+
+  return (
+    <div className='cursor-follower' style={{ left: `${position.x}px`, top: `${position.y}px` }}>
+      
+    </div>
+  );
+};
 
 export default CursorFollower;
